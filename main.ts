e3d0db427ff0f4ab22ef02ae63c421d78d6aee43
lcorrocher <l.a.corrocher@gmail.com>
2025-11-24 21:08:11 +0000

    right-click menu creates md/mdx files directly using relative vault paths

--- main.ts
+++ main.ts
@@ -1,70 +1,69 @@
-import {Plugin, normalizePath } from 'obsidian';
+import { Plugin, normalizePath, TAbstractFile, Menu } from 'obsidian';
 
 export default class MdxTools extends Plugin {
 
 	async onload() {
-		super.onload()
-
+		console.log("MdxTools plugin loaded");
 		this.registerExtensions(["mdx"], "markdown");
-		
-		this.addRibbonIcon('add-note-glyph', 'New .mdx file', (evt: MouseEvent) => {
-			this.createMDX()
-		});
 
 		this.registerEvent(
 			this.app.workspace.on("file-menu", (menu, file) => {
-				menu.addItem((item) => {
-					item
-					.setTitle("New .mdx file")
-					.setIcon("add-note-glyph")
-					.onClick(async () => {
-						let folder : string
-						const fname = file.path
-						if( fname.search("(\\.[^.]+)$") > 0  && file.parent) {
-							folder = file.parent.path
-						} else {
-							folder = fname
-						}
-						this.createMDX(folder)
-					});
-				});
+				this.addNewFileSubmenu(menu, file);
 			})
 		);
 
+		this.registerEvent(
+			this.app.workspace.on("folder-menu", (menu, folder) => {
+				this.addNewFileSubmenu(menu, folder);
+			})
+		);
+
+		this.addRibbonIcon("document", "Create test MDX", () => {
+        const folder = this.app.vault.getRoot().path;
+        this.createFile(folder, "mdx");
+        console.log("Test MDX file created in folder:", folder);
+    });
 	}
 
-	onunload() {
+	addNewFileSubmenu(menu: Menu, fileOrFolder: TAbstractFile) {
+		const folder = this.resolveFolder(fileOrFolder);
 
+		menu.addSeparator();
+		menu.addItem(item => {
+		item.setTitle("New .md file")
+			.onClick(() => this.createFile(folder, "md"));
+		});
+
+		menu.addItem(item => {
+	    item.setTitle("New .mdx file")
+		    .onClick(() => this.createFile(folder, "mdx"));
+		});
 	}
 
-	checkExists(filepath : string) {
-		return this.app.vault.getAbstractFileByPath(filepath) && true;
-	}
-
-	// Create new MDX file
-	createMDX(folder? : string) {
-		if ( !folder ) {
-			folder = this.app.fileManager.getNewFileParent(this.app.workspace.getActiveFile()?.path || '').path
+	resolveFolder(fileOrFolder: TAbstractFile): string {
+		if ("children" in fileOrFolder) {
+			return fileOrFolder.path;
 		}
-		
-		let filename = normalizePath(folder + "/Untitled.mdx")
+		return fileOrFolder.parent?.path ?? '/';
+	}
 
-		if (!this.checkExists(filename)) {
-			this.app.vault.create(filename, "")
-		} 
-		else { // If Untitled.mdx already exists, try Untitled 1.mdx etc
-			let iter = 0
-			while (true) {
-				iter = iter + 1
-				filename = normalizePath(folder + "/Untitled " + iter + ".mdx")
-				if (!this.checkExists(filename)) {
-					this.app.vault.create(filename, "")
-					break
-				}
+	createFile(folder: string, ext: "md" | "mdx") {
+		let base = `Untitled.${ext}`;
+		let filepath = normalizePath(`${folder}/${base}`);
+
+		if (!this.app.vault.getAbstractFileByPath(filepath)) {
+			this.app.vault.create(filepath, "");
+			return;
+		}
+
+		let i = 1;
+		while (true) {
+			filepath = normalizePath(`${folder}/Untitled ${i}.${ext}`);
+			if (!this.app.vault.getAbstractFileByPath(filepath)) {
+				this.app.vault.create(filepath, "");
+				break;
 			}
+			i++;
 		}
-
-
 	}
 }
-
